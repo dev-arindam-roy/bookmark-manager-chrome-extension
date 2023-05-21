c3bb718a2e9ad95e1bfba71ab930417cdd314414
Ari-Developer <arindam.roy.developer@gmail.com>
2023-05-22 01:44:20 +0530

    update

--- bookmark-script.js
+++ bookmark-script.js
@@ -1,3 +1,8 @@
+/**
+ * Developer Name: Arindam Roy
+ * arindam.roy.developer@gmail.com
+ * https://github.com/dev-arindam-roy
+ */
 const allBtn = document.querySelector('#allBtn');
 const searchBtn = document.querySelector('#searchBtn');
 const recentBtn = document.querySelector('#recentBtn');
@@ -6,6 +11,8 @@ const downloadBtn = document.querySelector('#downloadBookmarkCsv');
 
 const searchKeyword = document.querySelector('#searchTxtb');
 const infoText = document.querySelector('#infoHolder');
+
+const removeSvg = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#f10101" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"></circle><line x1="15" y1="9" x2="9" y2="15"></line><line x1="9" y1="9" x2="15" y2="15"></line></svg>`;
 
 var bookmarObject = new Array();
 
@@ -87,7 +94,7 @@ async function allBookmarks() {
 function fetchBookmarks(parentNode) {
     parentNode.forEach(function(bookmark) {
         if(!(bookmark.url === undefined || bookmark.url === null)) {
-            bookmarObject.push({title: bookmark.title, url: bookmark.url, createdAt: formatDate(bookmark.dateAdded)});
+            bookmarObject.push({id: bookmark.id, title: bookmark.title, url: bookmark.url, createdAt: formatDate(bookmark.dateAdded)});
         }
         if (bookmark.children) {
             fetchBookmarks(bookmark.children);
@@ -118,7 +125,7 @@ function getRecentBookmarks() {
             chrome.bookmarks.getRecent(100, (results) => {
                 results.forEach(function(bookmark) {
                     if(!(bookmark.url === undefined || bookmark.url === null)) {
-                        recentBookmarObject.push({title: bookmark.title, url: bookmark.url, createdAt: formatDate(bookmark.dateAdded)});
+                        recentBookmarObject.push({id: bookmark.id, title: bookmark.title, url: bookmark.url, createdAt: formatDate(bookmark.dateAdded)});
                     }
                 });
                 resolve(JSON.stringify(recentBookmarObject));
@@ -137,11 +144,23 @@ function getSearchBookmarks(searchKeywordText) {
             chrome.bookmarks.search(searchKeywordText, (results) => {
                 results.forEach(function(bookmark) {
                     if(!(bookmark.url === undefined || bookmark.url === null)) {
-                        searchBookmarObject.push({title: bookmark.title, url: bookmark.url, createdAt: formatDate(bookmark.dateAdded)});
+                        searchBookmarObject.push({id: bookmark.id, title: bookmark.title, url: bookmark.url, createdAt: formatDate(bookmark.dateAdded)});
                     }
                 });
                 resolve(JSON.stringify(searchBookmarObject));
             });
+        } catch(error) {
+            reject('Something Went Wrong!! Please Try Again...');
+        }
+    });
+}
+
+/** Promise for remove bookmark */
+function removeBookmarkById(bookmarkId) {
+    return new Promise((resolve, reject) => {
+        try {
+            chrome.bookmarks.remove(bookmarkId);
+            resolve(bookmarkId);
         } catch(error) {
             reject('Something Went Wrong!! Please Try Again...');
         }
@@ -169,6 +188,7 @@ function loadBookmarks(jsonString, actionType = null) {
                         <td>
                             <span class="bookmark-title">${item.title}</span><br/>
                             <a href="${item.url}" class="bookmark-link" target="_blank">${item.url}</a>
+                            <a href="#" class="remove-btn remove-bookmark-btn" id="removeBookmark_${index}" data-bookmark-id="${item.id}">${removeSvg}</a>
                         </td>
                         <td><span class="bookmark-date">${item.createdAt}</span></td>
                     </tr>`;
@@ -205,6 +225,49 @@ function loadErrors(errorString) {
 /** No records rendering - UI */
 function noRecordsFound() {
     result.innerHTML = `<tr><td colspan="2">Oops! I'm very Sorry, No Bookmarks Found.</td></tr>`;
+}
+
+/** Get remove bookmark btn event dynamically */
+document.querySelector('body').addEventListener('click', function(event) {
+    const tagName = event.target.tagName.toLowerCase();
+    const parentElement = event.target.parentNode;
+    const parentElementTagName = parentElement.tagName.toLowerCase();
+    if (tagName === 'svg' && parentElementTagName === 'a') {
+        const parentElementClassLsit = parentElement.classList.value.split(' ');
+        if (parentElementClassLsit.includes('remove-bookmark-btn')) {
+            if (confirm('Are you want to delete?')) {
+                const removeBtnId = parentElement.id;
+                if (removeBtnId) {
+                    const rowIndex = removeBtnId.split('_')[1];
+                    const bookmarkId = parentElement.getAttribute('data-bookmark-id');
+                    if (rowIndex && bookmarkId) {
+                        infoText.innerHTML = 'Bookmark deleting...';
+                        removeLoading(rowIndex);
+                        setTimeout(() => {
+                            removeBookmark(rowIndex, bookmarkId);
+                        }, 2000);
+                    }
+                }
+            }
+        }
+    }
+});
+
+/** Remove the bookmark */
+async function removeBookmark(rowIndex, bookmarkId) {
+    await removeBookmarkById(bookmarkId).then((resolveResp) => {
+        if (bookmarkId == resolveResp) {
+            document.getElementById('bookmarkRow' + rowIndex).remove();
+            infoText.innerHTML = `<span style="color: red;">Bookmark Id - ${resolveResp} has been deleted</span>`;
+        }
+    }).catch((rejectResp) => {
+        loadErrors(rejectResp);
+    });
+}
+
+/** display remove loading placeholder */
+function removeLoading(rowIndex) {
+    document.getElementById('bookmarkRow' + rowIndex).innerHTML = `<td colspan="2">${rowLoadingPlaceholder()}</td>`;
 }
 
 /** Download as CSV - json object */
@@ -255,6 +318,19 @@ function displayPlaceholder() {
                 <div class="ph-col-6 empty"></div>
                 <div class="ph-col-4"></div>
                 <div class="ph-col-8 empty"></div>
+                <div class="ph-col-6"></div>
+                <div class="ph-col-6 empty"></div>
+                <div class="ph-col-12"></div>
+            </div>
+        </div>
+    </div>`;
+}
+
+/** row loading placeholder */
+function rowLoadingPlaceholder() {
+    return `<div class="ph-item tr-loading-placeholder">
+        <div class="ph-col-12">
+            <div class="ph-row">
                 <div class="ph-col-6"></div>
                 <div class="ph-col-6 empty"></div>
                 <div class="ph-col-12"></div>
